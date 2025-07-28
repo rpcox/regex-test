5eb69863944fa880d4ac50ab8a4aedd3d430338d
Ric Cox <ric.cox@gmail.com>
2025-07-27 17:26:04 -0700

    add -alpha/-desc

--- re2/go.mod
+++ re2/go.mod
@@ -2,4 +2,4 @@ module github.com/rpcox/regex-test/re2
 
 go 1.21.5
 
-require github.com/rpcox/pkg/exit v0.1.3
+require github.com/rpcox/pkg/exit v0.3.4
--- re2/main.go
+++ re2/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -52,14 +53,21 @@ func HelpVerCheck(v, h bool) {
 	}
 }
 
-func LoadRegexList(fileName string) (map[string]*regexp.Regexp, map[string]int, *[]string) {
+type RegexData struct {
+	Elapsed  time.Duration  // duration of time to match on regex Name
+	HitCount int64          // number of hits for a given regex
+	Name     string         // name of the regex
+	Re       *regexp.Regexp // RE2 regex defined for Name
+	Sequence int            // the ordinal number (position) of the regex in rule list
+}
+
+func LoadRegexList(fileName string) (map[string]*RegexData, *[]string) {
 	regexFh, err := os.OpenFile(fileName, os.O_RDONLY, 0640)
-	exit.IfErr(err != nil, err, 1)
+	exit.IfErr(err, 1)
 	defer regexFh.Close()
 
 	lineCount := 0
-	r := make(map[string]*regexp.Regexp)
-	seq := make(map[string]int)
+	m := make(map[string]*RegexData)
 	var sequencedRegexes []string
 
 	comment := regexp.MustCompile(`^#`)
@@ -70,38 +78,62 @@ func LoadRegexList(fileName string) (map[string]*regexp.Regexp, map[string]int, 
 			continue
 		}
 		lineCount++
+		var rd RegexData
 		s := strings.Split(line, "\t")
 		exit.If(len(s) < 2, "err regex #"+strconv.Itoa(lineCount)+": "+line, 1)
-		r[s[0]] = regexp.MustCompile(s[1])
-		seq[s[0]] = lineCount
+		rd.Re = regexp.MustCompile(s[1])
+		rd.Sequence = lineCount
+		m[s[0]] = &rd
 		sequencedRegexes = append(sequencedRegexes, s[0])
 	}
 
-	return r, seq, &sequencedRegexes
+	return m, &sequencedRegexes
+}
+
+func SortByKey(m map[string]*RegexData) *[]string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+
+	sort.Strings(keys)
+
+	return &keys
+}
+
+func SortByHitCount(m map[string]*RegexData) *[]string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		return m[keys[i]].HitCount > m[keys[j]].HitCount
+	})
+
+	return &keys
 }
 
 func main() {
-	/*	_alpha := flag.Bool("alpha", false, "Results will be printed with regex names from REGEX_LIST listed alphabetically")
-		_desc := flag.Bool("desc", false, "Results will be printed with hit counts listed in descending order")
-	*/
+	_alpha := flag.Bool("alpha", false, "Results will be printed with regex names from REGEX_LIST listed alphabetically")
+	_desc := flag.Bool("desc", false, "Results will be printed with hit counts listed in descending order")
 	_data := flag.String("data", "", "Specify the location of the data set")
 	_regex := flag.String("regex", "", "Specify the location of the regex list")
-	_nomatch := flag.String("nomatch", "unmatched.txt", "Specify the location to place unmatched data")
+	_unmatch := flag.String("unmatch", "unmatched.txt", "Specify the location to place unmatched data")
 	_help := flag.Bool("help", false, "Display usage and exit")
 	_ver := flag.Bool("version", false, "Display version and exit")
 	flag.Parse()
 	HelpVerCheck(*_ver, *_help)
 
-	umFh, err := os.OpenFile(*_nomatch, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0640)
-	exit.IfErr(err != nil, err, 1)
+	umFh, err := os.OpenFile(*_unmatch, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0640)
+	exit.IfErr(err, 1)
 	defer umFh.Close()
 	dataFh, err := os.OpenFile(*_data, os.O_RDONLY, 0640)
-	exit.IfErr(err != nil, err, 1)
+	exit.IfErr(err, 1)
 	defer dataFh.Close()
 	scanner := bufio.NewScanner(dataFh)
 
-	testReg, seq, sequencedRegexes := LoadRegexList(*_regex)
-	stats := make(map[string]int)
+	regexMap, regexSequence := LoadRegexList(*_regex)
 	lineCount, matchCount, unmatchedCount := 0, 0, 0
 	start := time.Now()
 
@@ -109,9 +141,11 @@ NEXT_LINE:
 	for scanner.Scan() {
 		line := scanner.Text()
 		lineCount++
-		for _, regName := range *sequencedRegexes {
-			if s := testReg[regName].FindString(line); s != "" {
-				stats[regName]++
+		lineStart := time.Now()
+		for _, regName := range *regexSequence {
+			if s := regexMap[regName].Re.FindString(line); s != "" {
+				regexMap[regName].Elapsed += time.Since(lineStart)
+				regexMap[regName].HitCount++
 				matchCount++
 				goto NEXT_LINE
 			}
@@ -122,16 +156,40 @@ NEXT_LINE:
 	}
 
 	elapsed := time.Since(start)
-	fmt.Printf("%8d : data lines read in\n", lineCount)
-	fmt.Printf("%8d : matched lines\n", matchCount)
-	fmt.Printf("%8d : unmatched lines\n\n", unmatchedCount)
-	fmt.Println(" SEQ    COUNT  REGEX")
 
-	//for i, regName := range *sequencedRegexes {
-	for _, regName := range *sequencedRegexes {
-		//	fmt.Printf("%4d %8d  %s\n", i, stats[regName], regName)
-		fmt.Printf("%4d %8d  %s\n", seq[regName], stats[regName], regName)
+	var matchSum int64
+	for _, v := range regexMap {
+		if v.HitCount > 0 {
+			matchSum += v.HitCount
+		}
 	}
 
-	fmt.Println("elapsed:", elapsed)
+	fmt.Printf("SUMMARY\n\n")
+	fmt.Printf("  run date : %20s\n", time.Now().UTC().Format(time.RFC3339))
+	fmt.Printf("%10.6f : elapsed (seconds)\n", elapsed.Seconds())
+	fmt.Printf("%10d : data lines read in\n", lineCount)
+	fmt.Printf("%10d : regexes loaded\n", len(*regexSequence))
+	fmt.Printf("%10d : matched lines\n", matchSum)
+	if matchSum != int64(matchCount) {
+		fmt.Fprintf(os.Stderr, "\t!!matchSum (%d) != matchCount (%d)\n", matchSum, matchCount)
+	}
+	fmt.Printf("%10d : unmatched lines\n\n", unmatchedCount)
+	fmt.Printf("MATCHES\n\n")
+	fmt.Printf("%30s %8s %8s %11s\n", "REGEX", "ORD", "HITS", "DURATION")
+
+	if *_alpha {
+		for _, regName := range *SortByKey(regexMap) {
+			fmt.Printf("%30s %8d %8d    %10.6f\n", regName, regexMap[regName].Sequence, regexMap[regName].HitCount, regexMap[regName].Elapsed.Seconds())
+		}
+
+	} else if *_desc {
+		for _, regName := range *SortByHitCount(regexMap) {
+			fmt.Printf("%30s %8d %8d    %10.6f\n", regName, regexMap[regName].Sequence, regexMap[regName].HitCount, regexMap[regName].Elapsed.Seconds())
+		}
+	} else {
+		for _, regName := range *regexSequence {
+			fmt.Printf("%30s %8d %8d    %10.6f\n", regName, regexMap[regName].Sequence, regexMap[regName].HitCount, regexMap[regName].Elapsed.Seconds())
+		}
+	}
+	fmt.Println()
 }
